d36a388222a1bd2f453fc633daf8db1beec9032f
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-06-30 17:02:40 +0530

    Time: 26 ms (11.02%), Space: 10.8 MB (59.74%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int partition(vector<int>& nums, int l, int h)
     {
-
+        int num = (l+h)/2;
+        swap(nums[h], nums[num]);
         int i=l,j,pi=h;
         for(j=l; j<h; j++)
         {
@@ -49,7 +50,6 @@ public:
         
         
         int median = quick(nums,0,n-1,n/2);
-       
         for (int i=0;i<nums.size();i++) sum += abs(nums[i] - median);
         return sum;
 
