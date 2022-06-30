fe2714ef7e64eefc41b89143263576174685f725
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-06-30 16:32:38 +0530

    Time: 461 ms (5.90%), Space: 381.9 MB (5.28%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,31 +1,41 @@
 class Solution {
 public:
     
-    long long findDiff(vector<int> nums, long long i)
-    {
-        long long sum = 0;
-        for(auto it: nums)
-        {
-            if(it == i) continue;
-            else sum+=abs(it-i);
+    int partition(vector<int> nums, int low, int high){
+        int pivot = nums[high];
+        int pos = low;
+        
+        for(int i=low; i<=high; i++){
+            if(nums[i]<pivot){
+                swap(nums[i], pivot);
+                pos++;
+            }
         }
-        return sum;
+        
+        swap(nums[pos], nums[high]);
+        return pos;
+    }
+    
+    int findMedian(vector<int>& nums,int k, int low, int high)
+    {
+        int pos = partition(nums, low, high);
+        if(pos==k-1) return nums[pos];
+        
+        if(pos<k-1) return findMedian(nums, k, pos+1, high);
+        else return findMedian(nums, k, low, pos-1);
     }
     int minMoves2(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         
-        int n = nums.size();
+        int n = nums.size(), sum=0;
         
         if(n==2) return abs(nums[0]-nums[1]);
         
-        if(n%2){
-            return findDiff(nums, nums[n/2]);
-        }
-       
-        return min({
-            findDiff(nums, nums[n/2]), findDiff(nums, nums[n/2 + 1])
-        });
-       
+        int median = findMedian(nums, n/2 + 1,0,n-1);
+        //cout<<median;
+        for (int i=0;i<nums.size();i++) sum += abs(nums[i] - median);
+        return sum;
+
         
     }
 };
