ef4d4b4e061cac2b5d25c3da2f225b8b3a72e236
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-07-04 16:45:18 +0530

    Time: 1045 ms (13.46%), Space: 93.2 MB (83.08%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -14,18 +14,13 @@ public:
     int minimumEffort(vector<vector<int>>& t) {
         sort(t.begin(), t.end(), [](vector<int> &t1, vector<int> &t2) 
          { return t1[1] - t1[0] > t2[1] - t2[0]; });
-        int r = 0, l= 0;
-        for(vector<int> it:t) {
-            r += it[1];
-            l = max(l, it[0]);
-        }
+        int r = 1000000000, l= 1;
+
         //cout<<l<<" "<<r<<endl;
         int mid;
         while(l<r) {
             mid = (l+r)>>1;
-            //cout<<mid<<endl;
             if(isPossible(t, mid)) {
-                //cout<<"true "<<mid<<endl;
                 r = mid;
             }
             else l = mid+1;
