c5ba7ca8180395447f2becb3a35048da5943ff55
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-07-04 15:29:59 +0530

    Time: 32 ms (55.29%), Space: 17.8 MB (65.79%) - LeetHub

--- 135-candy/135-candy.cpp
+++ 135-candy/135-candy.cpp
@@ -5,19 +5,19 @@ public:
         int n = arr.size();
         vector<int> out(n, 1);
         
+        int j, curr;
         int sum = 0;
         for(int i=0; i<n-1;)
         {
             if(i+1<n && arr[i+1]>arr[i]) {
                 out[i+1] = out[i]+1;
- 
                 i++;
             }
             else if(i+1<n && arr[i+1]<arr[i])
             {
-                int j = i;
+                j = i;
                 while(j+1<n && arr[j+1]<arr[j]) j++;
-                int curr = j-i+1;
+                curr = j-i+1;
                 out[i] = max(out[i], curr); 
                 j = i+1;
                 while(j+1<n && arr[j+1]<arr[j]) {
