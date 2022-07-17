4afdbb5af32db0c283ebd11a13c6434202a09583
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-07-17 17:16:06 +0530

    Time: 2388 ms (5.13%), Space: 10.3 MB (75.64%) - LeetHub

--- 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
+++ 629-k-inverse-pairs-array/629-k-inverse-pairs-array.cpp
@@ -1,21 +1,11 @@
 class Solution {
 public:
-    int memo[1001][1001];
-    int solve(int n, int k){
-        if (n == 0)
-            return 0;
-        if (k == 0)
-            return 1;
-        if (memo[n][k] != -1)
-            return memo[n][k];
-        int inv = 0;
-        for (int i = 0; i <= min(k, n - 1); i++)
-            inv = (inv + solve(n - 1, k - i)) % 1000000007;
-        
-        return memo[n][k] = inv;
-    }
     int kInversePairs(int n, int k) {
-        memset(memo, -1, sizeof(memo));
-        return solve(n, k);
-    }
+    int dp[1001][1001] = { 1 };
+    for (int N = 1; N <= n; ++N)
+        for (int K = 0; K <= k; ++K)
+            for (int i = 0; i <= min(K, N - 1); ++i) 
+                dp[N][K] = (dp[N][K] + dp[N - 1][K - i]) % 1000000007;
+    return dp[n][k];
+}
 };
