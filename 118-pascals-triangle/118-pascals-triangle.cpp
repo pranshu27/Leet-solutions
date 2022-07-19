692a0cc75a92d1ae32f85d71e144122a5c11cc66
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-07-19 11:12:32 +0530

    Time: 6 ms (8.92%), Space: 6.4 MB (71.06%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -5,12 +5,12 @@ public:
         out.push_back(vector<int>{1});
         if(numRows==1) return out;
         if(numRows>1) out.push_back(vector<int>{1,1});
-        
+        vector<int> tmp;
         for(int i=0; i<numRows-2; i++)
         {
             vector<int> &prev = out.back();
             //for(auto it: prev) cout<<it<<" "; cout<<endl;
-            vector<int> tmp;
+            
             tmp.push_back(1);
             for(int j = 0; j<prev.size()-1; j++)
             {
@@ -19,7 +19,7 @@ public:
             tmp.push_back(1);
            // for(auto it: tmp) cout<<it<<" "; cout<<endl;
             out.push_back(tmp);
-            //prev.clear();
+            tmp.clear();
            
         }
         
