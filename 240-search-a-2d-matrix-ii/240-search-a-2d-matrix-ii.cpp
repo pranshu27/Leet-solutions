f8c4f822ea575698f3236a7a93db83dcac37fcb8
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-07-24 15:04:13 +0530

    Time: 247 ms (25.07%), Space: 15 MB (21.86%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,46 +1,28 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-    int row = matrix.size(), col = row ? matrix[0].size() : 0;
-    int start = 0, end = row, mid, endRow, startRow;
-    // first we determine the first row the target might occurs.
-    // notice that here we implement the lower_bound binary search.
-    while (start < end) {
-        mid = (start + end) / 2;
-        if (matrix[mid][col-1] >= target) {
-            end = mid;
-        } else {
-            start = mid + 1;
-        }
-    }
-    startRow = start;
-    //then we caclulate the end row (not included) the target might occurs
-    // here is for upper_bound binary search.
-    start = 0, end = row;
-    while (start < end) {
-        mid = (start + end) / 2;
-        if (matrix[mid][0] > target) {
-            end = mid;
-        } else {
-            start = mid + 1;
-        }
-    }
-    endRow = start;
-    //finally, iterate through each row until we find the target
-    // and here is also lower_bound binary search.
-    for (int i = startRow;i < endRow;++i) {
-        start = 0, end = col;
-        while (start < end) {
-            mid = (start + end) / 2;
-            if (matrix[i][mid] >= target) {
-                end = mid;
-            } else {
-                start = mid + 1;
+        int up=0;
+        int left=0;
+        int down=matrix.size()-1;
+        int right=matrix[0].size()-1;        
+        while(up<=down&&left<=right)
+        {
+            if(matrix[up][right]==target||matrix[down][left]==target)return true;
+            if(matrix[up][right]<target){
+                up++;
+            }
+            else
+            {
+                right--;
+            }
+            if(matrix[down][left]<target){
+                left++;
+            }
+            else
+            {
+                down--;
             }
         }
-        if (start < col && matrix[i][start] == target)
-            return true;
+        return false;
     }
-    return false;
-}
 };
