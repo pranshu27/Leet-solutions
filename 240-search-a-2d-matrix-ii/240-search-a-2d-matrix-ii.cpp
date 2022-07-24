7ae4c7ba0f2764fee5904fbbfe1cabb8cf43aa2c
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-07-24 15:01:02 +0530

    Time: 241 ms (26.44%), Space: 15 MB (21.86%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,14 +1,46 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int m = matrix.size(),  n = matrix[0].size() ; 
-        
-        int i = 0, j = n-1;
-        
-        while(i<m && j>=0){
-            if(matrix[i][j] == target) return true;
-            matrix[i][j]>target? j-- : i++;
+    int row = matrix.size(), col = row ? matrix[0].size() : 0;
+    int start = 0, end = row, mid, endRow, startRow;
+    // first we determine the first row the target might occurs.
+    // notice that here we implement the lower_bound binary search.
+    while (start < end) {
+        mid = (start + end) / 2;
+        if (matrix[mid][col-1] >= target) {
+            end = mid;
+        } else {
+            start = mid + 1;
         }
-        return false;
     }
+    startRow = start;
+    //then we caclulate the end row (not included) the target might occurs
+    // here is for upper_bound binary search.
+    start = 0, end = row;
+    while (start < end) {
+        mid = (start + end) / 2;
+        if (matrix[mid][0] > target) {
+            end = mid;
+        } else {
+            start = mid + 1;
+        }
+    }
+    endRow = start;
+    //finally, iterate through each row until we find the target
+    // and here is also lower_bound binary search.
+    for (int i = startRow;i < endRow;++i) {
+        start = 0, end = col;
+        while (start < end) {
+            mid = (start + end) / 2;
+            if (matrix[i][mid] >= target) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        if (start < col && matrix[i][start] == target)
+            return true;
+    }
+    return false;
+}
 };
