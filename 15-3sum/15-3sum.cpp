2526277e8e207b9244390d578a969c92f8abefec
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-07-30 19:53:42 +0530

    Time: 85 ms (91.85%), Space: 19.8 MB (92.74%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,38 +1,38 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> out;
-        int n = nums.size();
-        sort(nums.begin(), nums.end());
-        for(int i=0; i<n-2; i++)
-        { 
-            if(nums[i]>0) break;
-            if(i>0 && nums[i]==nums[i-1]&& out.size()!=0) {
-                continue;
-            }
-            for(int j=i+1, k = n-1; j<k; ){
-                if(j>i+1 &&nums[j-1] == nums[j] && out.size()!=0) {
-                    j++;
-                   // cout<<"here\n";
-                    continue;
-                }
-                    //cout<<"h\n";
-                if(k<n-1 && nums[k] == nums[k+1] && out.size()!=0)
-                {
-                    k--;
-                    continue;
-                }
-                if(nums[i]+nums[j]+nums[k]==0) {
-                    out.push_back({nums[i],nums[j],nums[k]});
-                    j++; k--;
-                }
-                else if(nums[i]+nums[j]+nums[k] < 0) j++;
-                else k--;
-            }
-        }
-        
-        return out;
-        //return  vector<vector<int>>(out.begin(), out.end());
+        std::vector<vector<int>> result;
+	if (nums.empty()) {
+		return result;
+	}
+
+	std::size_t n_size = nums.size();
+	std::sort(nums.begin(), nums.end());
+	for (int i = 0; i < n_size; ++i) {
+		// all numbers from now on will be greater than 0, no point in continuing
+		if (nums[i] > 0) break;
+
+		// we have seen this number & combo before; skip
+		if (i > 0 and nums[i] == nums[i-1]) continue;
+
+		int left = i+1, right = n_size - 1;
+		while (left < right) {
+			int sum = nums[i] + nums[left] + nums[right];
+			if (sum < 0) {
+				++left;
+			} else if (sum > 0) {
+				--right;
+			} else {
+				result.push_back({nums[i], nums[left], nums[right]});
+				int last_left = nums[left], last_right = nums[right];
+				// we have seen this number & combo before; skip
+				while (left < right && nums[left] == last_left) ++left;
+				while (left < right && nums[right] == last_right) --right;
+			}
+		}
+
+	    }
+	    return result;
         
     }
 };
