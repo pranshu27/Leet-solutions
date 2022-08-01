5d260fcac264146c11719a57c5bd47343e975f18
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-08-01 16:13:24 +0530

    Time: 0 ms (100.00%), Space: 6.6 MB (26.54%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -13,7 +13,18 @@ public:
         return dp[i][j] = solve(i+1, j, m, n, dp) + solve(i, j+1, m, n, dp);
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(100, vector<int>(100, -1));
-        return solve(0, 0, m, n, dp);
+       vector<vector<int>> dp(m, vector<int>(n, 1));
+		
+        // Many may confuse by seeing the above line 
+        // This is same as dp[m][n] = {0} using array
+        
+        // iterate to all the grids in m*n and set dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+        // Finally result the last grid ans
+        return dp[m - 1][n - 1];
     }
 };
