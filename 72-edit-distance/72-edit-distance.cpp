490c36edf80eec0f1602bd52342446da4bd357f7
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-08-02 21:02:26 +0530

    Time: 66 ms (21.51%), Space: 8.3 MB (69.83%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -22,10 +22,36 @@ public:
     }
     
     int minDistance(string s, string t) {
-        memset(dp, -1, sizeof(dp));
-        int m = s.size();
-        int n = t.size();
-        
-        return f(s, t, m-1, n-1);
+        int n = s.size();
+        int m = t.size();
+        int dp[n+1][m+1];
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=m;j++)
+            {
+                if(i==0)
+                    dp[i][j] = j;
+                else if(j==0)
+                    dp[i][j] = i;
+            }
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(s[i-1]==t[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else{
+                    dp[i][j] = min(
+                        dp[i][j-1],
+                        min(
+                            dp[i-1][j],
+                            dp[i-1][j-1]
+                        )
+                    )+1;
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
