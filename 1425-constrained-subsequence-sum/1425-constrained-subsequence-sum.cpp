8033718153adbd9d6c7fa322beb9ad5fe1f24df4
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-08-04 03:13:59 +0530

    Time: 549 ms (40.75%), Space: 139.4 MB (32.66%) - LeetHub

---
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    
+    int solve(vector<int>& nums, int index, int prev_index, int &k, vector<vector<int>> &dp) {
+       
+        if(index == nums.size() || (prev_index!=-1 && index-prev_index>k))
+            return 0;
+        
+        if(dp[index][prev_index+1]!=-1) return dp[index][prev_index+1];
+
+        int take = 0, nottake = 0;
+        
+        if(prev_index==-1 || index-prev_index<=k)
+        {
+            take = dp[index+1][index+1] != -1? dp[index+1][index+1]: solve(nums, index+1, index,k, dp)+nums[index];
+        }
+        
+        nottake = dp[index+1][prev_index+1]!=-1? dp[index+1][prev_index+1]: solve(nums, index+1, prev_index, k, dp);
+        
+        return dp[index][prev_index+1] =  max(take, nottake);
+        
+    } 
+    int constrainedSubsetSum(vector<int>& nums, int k) {
+        priority_queue<array<int, 2>> que;
+        int ret = nums[0], curr;
+        que.push({nums[0], 0});
+        for (int i = 1; i < nums.size(); i++) {
+            while (!que.empty() && que.top()[1] < i - k) {
+                que.pop();
+            }
+            curr = max(0, que.top()[0]) + nums[i];
+		    ret = max(ret, curr);
+            que.push({curr, i});
+        }
+        return ret;
+
+//         int n = nums.size();
+//         vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
+        
+//         int ans = solve(nums, 0, -1, k, dp), maxx=INT_MIN+1;
+        
+//         if(ans!=0)
+//         return ans;
+
+//         for(int i=0; i<nums.size(); i++)
+//             maxx=max(maxx, nums[i]);
+        
+
+//          return maxx;
+    }
+};
