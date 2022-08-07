2ee796c782486afcbcb5287e8bd888be0be673fd
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-08-07 18:15:57 +0530

    Time: 22 ms (73.99%), Space: 6 MB (85.05%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -3,22 +3,25 @@ public:
     
     int countVowelPermutation(int n) {
         
-        vector<vector<long long>> dp(n, vector<long long>(5, 1));
+        vector<long long> curr(5, 1);
+        vector<long long> after(5, 1);
+        
         int mod = 1e9+7;
         for(int i=1; i<n; i++)
         {
 
-            dp[i][0] = (dp[i-1][4]%mod + dp[i-1][1]%mod + dp[i-1][2]%mod)%mod; //words ending at a
-            dp[i][1] = (dp[i-1][0]%mod + dp[i-1][2]%mod)%mod;
-            dp[i][2] = (dp[i-1][1]%mod + dp[i-1][3]%mod)%mod;
-            dp[i][3] = (dp[i-1][2]%mod)%mod;
-            dp[i][4] = (dp[i-1][2]%mod + dp[i-1][3]%mod)%mod;
+            curr[0] = (after[4]%mod + after[1]%mod + after[2]%mod)%mod; //words ending at a
+            curr[1] = (after[0]%mod + after[2]%mod)%mod;
+            curr[2] = (after[1]%mod + after[3]%mod)%mod;
+            curr[3] = (after[2]%mod)%mod;
+            curr[4] = (after[2]%mod + after[3]%mod)%mod;
+            after = curr;
 
         }
         
         long long ans = 0;
         for(int i=0; i<5; i++) {
-            ans += dp[n-1][i]%mod;
+            ans += curr[i]%mod;
             ans %= mod;
         }
         return ans%mod;
