343212c6045a28d6ca49cd4e797baea6dde2edf0
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-08-15 17:28:30 +0530

    Time: 36 ms (15.48%), Space: 8.9 MB (7.39%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,65 +1,25 @@
 class Solution {
 public:
-    int prior(char &ch)
-    {
-        if(ch=='M') return 7;
-        if(ch=='D') return 6;
-        if(ch=='C') return 5;
-        if(ch=='L') return 4;
-        if(ch=='X') return 3;
-        if(ch=='V') return 2;
-        return 1;
-    }
-    
-    int val(char &ch)
-    {
-        if(ch=='M') return 1000;
-        if (ch=='D') return 500;
-        if(ch=='C') return 100;
-        if(ch=='L') return 50;
-        if(ch=='X') return 10;
-        if(ch=='V') return 5;
-        return 1;
-        
-        
-    }
-    int romanToInt(string str) {
-        
-        int n = str.length();
-        int sum = 0;
-        
-        stack<int> st;
-        
-        st.push(val(str[n-1]));
-        
-        for(int i=n-2; i>=0; i--)
-        {
-           // cout<<sum<<endl;
-            int tmp_sum=0;
-            if(prior(str[i])>=prior(str[i+1]) || st.empty()) {
-                st.push(val(str[i]));
-                //cout<<"pushed "<<val(str[i])<<endl;
-            }
-            
-            else {
- 
-                tmp_sum = st.top()-val(str[i]);
-                st.pop();
-                
-                //sum+=tmp_sum;
-                st.push(tmp_sum);
-                //cout<<"pushed "<<tmp_sum<<endl;
-            }
-            
-        }
 
+    int romanToInt(string s) {
         
-        while(!st.empty()){
-            sum += st.top();
-            st.pop();
+        unordered_map<char,int> mp{
+        {'I',1},
+        {'V',5},
+        {'X',10},
+        {'L',50},
+        {'C',100},
+        {'D',500},
+        {'M',1000},
+        };
+        int ans =0;
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]]<mp[s[i+1]])
+                ans-=mp[s[i]];
+            else
+                ans+=mp[s[i]];
         }
-        
-        return sum;
+        return ans;
     
         
         
