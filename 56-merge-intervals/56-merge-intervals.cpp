da199f37d904e7102fe0661f7e55b9cb1024cf06
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-08-26 07:57:57 +0530

    Time: 73 ms (34.61%), Space: 19.2 MB (44.86%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -3,7 +3,7 @@ public:
 
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         
-        sort(intervals.begin(), intervals.end(), [](vector<int> &a, vector<int> &b){
+        sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int> &b){
             return (a[0]<b[0]);
         });
         //int size = intervals.size();
