d48b5e845b6411126f9647ca81110189201e427f
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-09-12 19:29:15 +0530

    Time: 1212 ms (5.00%), Space: 69.6 MB (7.07%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -5,19 +5,25 @@ public:
     {
         return a[1]<b[1];
     }
-    
+    int dp[1001][1001];
     int solve(int i, int j, vector<vector<int>>& pairs, int n)
     {
-        if(j>=n) return 0;
+        if(i==n || j==n || i>j) return 0;
         
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int take=0, nottake=0;
         if(pairs[i][1] < pairs[j][0])
         {
-            return 1+solve(j, j+1, pairs, n);
+            
+            take = 1+ ((dp[j][j+1]==-1)? solve(j, j+1, pairs, n): dp[j][j+1]);
         }
-        else return solve(i, j+1, pairs, n);
+        nottake = (dp[i][j+1]==-1)? solve(i, j+1, pairs, n): dp[i][j+1];
+        return dp[i][j] = max(take, nottake);
     }
     int findLongestChain(vector<vector<int>>& pairs) {
         sort(pairs.begin(), pairs.end(), comp);
+        memset(dp, -1, sizeof(dp));
         //for(auto a:pairs) cout<<a[0]<<" "<<a[1]<<endl;
         int n = pairs.size();
         return 1+solve(0, 1,pairs, n);
