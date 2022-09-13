ca4453ae61f5eb38ef7adb851098095c7bd68b1c
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-09-13 19:03:23 +0530

    Time: 2292 ms (5.07%), Space: 132.6 MB (16.37%) - LeetHub

--- 741-cherry-pickup/741-cherry-pickup.cpp
+++ 741-cherry-pickup/741-cherry-pickup.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int m; 
-    unordered_map<string ,int>p;
+    map<string ,int>p;
     int solve(int i1,int j1,int i2,int j2,vector<vector<int>>& grid)
     {
         if(i1>=m||i2>=m||j1>=m||j2>=m||grid[i1][j1]==-1||grid[i2][j2]==-1)
@@ -14,13 +14,17 @@ public:
         int temp=0; 
     string mp=to_string(i1)+"."+to_string(j1)+"."+to_string(i2)+"."+to_string(j2);
         if(p.count(mp)!=0)return p[mp];
+        string k1=to_string(i1+1)+"."+to_string(j1)+"."+to_string(i2+1)+"."+to_string(j2);
+        string k2=to_string(i1+1)+"."+to_string(j1)+"."+to_string(i2)+"."+to_string(j2+1);
+        string k3=to_string(i1)+"."+to_string(j1+1)+"."+to_string(i2+1)+"."+to_string(j2);
+        string k4=to_string(i1)+"."+to_string(j1+1)+"."+to_string(i2)+"."+to_string(j2+1);
         
         if(i1==i2&&j1==j2)temp=grid[i1][j1];
         else temp=grid[i1][j1]+grid[i2][j2];
-        int x1=solve(i1+1,j1,i2+1,j2,grid);
-        int x2=solve(i1+1,j1,i2,j2+1,grid);
-        int x3=solve(i1,j1+1,i2+1,j2,grid);
-        int x4=solve(i1,j1+1,i2,j2+1,grid);
+        int x1=p.count(k1)==0? solve(i1+1,j1,i2+1,j2,grid): p[k1];
+        int x2=p.count(k2)==0?solve(i1+1,j1,i2,j2+1,grid): p[k2];
+        int x3=p.count(k3)==0?solve(i1,j1+1,i2+1,j2,grid): p[k3];
+        int x4=p.count(k4)==0?solve(i1,j1+1,i2,j2+1,grid): p[k4];
         
         return p[mp]=temp+max({x1,x2,x3,x4});
         
