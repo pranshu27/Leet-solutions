9d422d8661a51c7160a53ae7d32fb4efabed0ab6
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-09-17 18:38:58 +0530

    Time: 26 ms (58.70%), Space: 15.5 MB (85.85%) - LeetHub

--- 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
+++ 95-unique-binary-search-trees-ii/95-unique-binary-search-trees-ii.cpp
@@ -1,25 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
 class Solution {
 public:
-    vector<TreeNode*> allTrees(int l,int r){
+    vector<TreeNode *> solve(int l, int r)
+    {
         if(l>r) return {NULL};
         if(l==r) return {new TreeNode(l)};
-        
         vector<TreeNode*> ans;
-        
-        for(int val=l;val<=r;val++){
-            vector<TreeNode*> leftSubtrees = allTrees(l,val-1);
-            vector<TreeNode*> rightSubtrees = allTrees(val+1,r);
-            for(TreeNode* leftSubtree: leftSubtrees){
-                for(TreeNode* rightSubtree: rightSubtrees){
-                    ans.push_back(new TreeNode(val,leftSubtree,rightSubtree));
-                }
-            }
+        for(int tmp = l;  tmp <=r; tmp++){
+            vector<TreeNode *> left = solve(l, tmp-1);
+            vector<TreeNode *> right = solve(tmp+1, r);
+            
+            for(auto l: left)
+                for(auto r: right)
+                    ans.push_back({new TreeNode(tmp, l, r)});
+            
         }
         
         return ans;
+        
     }
-    
     vector<TreeNode*> generateTrees(int n) {
-        return allTrees(1,n);
+        return solve(1,n);
     }
 };
