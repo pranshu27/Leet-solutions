e1f32f4042d620192f077765ea1768fbe5510140
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-09-17 18:16:46 +0530

    Time: 28 ms (5.50%), Space: 17.5 MB (5.14%) - LeetHub

---
+++ 863-all-nodes-distance-k-in-binary-tree/863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    
+    void buildGraph(TreeNode *root, map<TreeNode *, vector<TreeNode *>> &adj)
+    {
+        if(!root) return;
+        
+        if(root->left) {
+            adj[root].push_back(root->left);
+            adj[root->left].push_back(root);
+            buildGraph(root->left, adj);
+        }
+        
+        if(root->right) {
+            adj[root].push_back(root->right);
+            adj[root->right].push_back(root);
+            buildGraph(root->right, adj);
+        }   
+    }
+    
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        map<TreeNode *, vector<TreeNode *>> adj;
+        buildGraph(root, adj);
+        
+        if(k==0) return vector<int>{target->val};
+        
+        map<TreeNode *, int> visited;
+        vector<int> out;
+        queue<TreeNode *> q;
+        q.push(target);
+        //visited[target]
+        int dist = 0;
+        while(!q.empty())
+        {
+            int n = q.size();
+            while(n--)
+            {
+                TreeNode *node = q.front(); 
+                q.pop();
+                visited[node] = 1;
+                for(auto it: adj[node])
+                {
+                    if(!visited[it]) {
+                        q.push(it);
+                    }
+                }
+            }
+            dist++;
+            if(dist==k)
+            {
+                while(!q.empty())
+                {
+                    out.push_back(q.front()->val); 
+                    q.pop();
+                }
+                break;
+            }
+            
+        }
+        
+        return out;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+};
