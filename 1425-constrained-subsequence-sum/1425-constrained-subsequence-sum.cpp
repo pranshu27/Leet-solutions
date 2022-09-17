cd33c30281274240ba8fd0d62478117536676f57
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-09-17 20:21:47 +0530

    Time: 512 ms (55.48%), Space: 119.7 MB (71.99%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.cpp
@@ -4,19 +4,18 @@ public:
         int n = nums.size();
 		deque<int> d;
         vector<int> dp(n, 0);
+        int maxx = INT_MIN;
 
         for(int i=0; i<n; i++)
         {
             while(!d.empty() && i-d.front()>k) d.pop_front();
             dp[i]=nums[i];
             if(!d.empty()) dp[i] = max(dp[i], dp[i]+dp[d.front()]);
-            
+            maxx = max(maxx, dp[i]);
             while(!d.empty() && dp[d.back()]<=dp[i]) d.pop_back();
             d.push_back(i);
         }
-        
-        int maxx = INT_MIN;
-        for(auto &it: dp) maxx = max(maxx, it);
+
         return maxx;
     }
 };
