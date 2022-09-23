50af8a75ba0c0108ca89dbd234ed700307fc6c58
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-09-23 06:17:13 +0530

    Time: 10 ms (33.88%), Space: 7.5 MB (54.48%) - LeetHub

--- 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
+++ 1442-count-triplets-that-can-form-two-arrays-of-equal-xor/1442-count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -9,13 +9,7 @@ public:
         
         for (int i = 0; i < n; ++i)
             for (int k = i + 1; k < n; ++k)
-                // j ranges from i + 1 to k
-                // we check if xors[j] ^ xors[i] == xors[k + 1] ^ xors[j]
-					// property of xor: x ^ x = 0, 0 ^ x = x ^ 0 = x
-					// Thus, a_i ^ ... ^ a_j = (a_1 ^ ... ^ a_(i - 1)) ^ (a_1 ^ ...^ a_(i - 1) ^ a_i ^ ... ^ a_j)
-					//                       = xors[i - 1] ^ xors[j].
-                // which is equivalent to xors[i] == xors[k + 1]
-                // if that is true, it is true for every j, and we increment ans by k - i
+
                 if (xors[i] == xors[k+1])
                     ans += k - i;
         return ans;
