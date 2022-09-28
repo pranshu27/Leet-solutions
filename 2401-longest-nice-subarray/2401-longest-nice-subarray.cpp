0dede765e5cda736475466515ec203c29b3be9db
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-09-29 01:26:05 +0530

    Time: 470 ms (29.33%), Space: 66.1 MB (9.46%) - LeetHub

--- 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
+++ 2401-longest-nice-subarray/2401-longest-nice-subarray.cpp
@@ -2,27 +2,29 @@ class Solution {
 public:
     int longestNiceSubarray(vector<int>& nums) {
         int n = nums.size();
-
-
+        vector<long long> x(n), pre(n);
+        x[0] = pre[0] = nums[0];
+        for(int i = 1; i<n; i++)
+        {
+            pre[i]=pre[i-1]+nums[i];
+            x[i]=x[i-1]^nums[i];
+        }
         
         int maxx = 1;
-        
-        
+        int start = 0, end = 1;
+//    
         for(int i=0; i<n; i++)
         {
-            int len = 1;
-            int tmp = nums[i];
-            for(int j = i; j<n-1; j++)
+            for(int j = i+1; j<n; j++)
             {
-                if((tmp^nums[j+1]) == (tmp+nums[j+1]))
-                {
-                    tmp = tmp+nums[j+1];
-                    len++;
+                long long a = i>0? (pre[j]-pre[i-1]): pre[j];
+                long long b = i>0? (x[j]^x[i-1]): x[j];
+                if(a==b){
+                    maxx = max(maxx, j-i+1);
                 }
-                
                 else break;
+                
             }
-            maxx = max(len, maxx);
         }
         
         return maxx;
