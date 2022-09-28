3cd85b63dd1566a125d55003bf4616da0dea2199
Pranshu Sahijwani <pranshu.sahijwani@hotmail.com>
2022-09-28 21:30:30 +0530

    Time: 605 ms (20.87%), Space: 233.9 MB (12.82%) - LeetHub

--- 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.cpp
+++ 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.cpp
@@ -1,38 +1,51 @@
 class Solution {
 public:
-    vector<int> seg;
-    //Segment tree to return maximum in a range
-    void upd(int ind, int val, int x, int lx, int rx) {
-        if(lx == rx) {
-            seg[x] = val;
+    
+    int query(int ss, int se, int qs, int qe, int index, vector<int>& tree){
+        if(qs<=ss && se<=qe) return tree[index];
+        if(qe < ss || qs > se) return 0;
+        
+        int mid = (ss+se)>>1;
+        int left =  query(ss, mid, qs, qe, 2*index+1, tree);
+        int right =  query(mid+1, se, qs, qe, 2*index+2, tree);
+        
+        return max(left, right);
+        
+    }
+    void update(int ss, int se, int i, int inc, int index,vector<int>& tree)
+    {
+        if(i<ss || i>se) return;
+        
+        if(ss == se) {
+            tree[index]=inc;
             return;
         }
-        int mid = lx + (rx - lx) / 2;
-        if(ind <= mid)
-            upd(ind, val, 2 * x + 1, lx, mid);
-        else 
-            upd(ind, val, 2 * x + 2, mid + 1, rx);
-        seg[x] = max(seg[2 * x + 1], seg[2 * x + 2]);
+        
+        int mid = (ss+se)>>1;
+        update(ss, mid, i, inc, 2*index+1, tree);
+        update(mid+1, se, i, inc, 2*index+2, tree);
+        
+        tree[index] = max(tree[2*index+1], tree[2*index+2]);
+        
     }
-    int query(int l, int r, int x, int lx, int rx) {
-        if(lx > r or rx < l) return 0;
-        if(lx >= l and rx <= r) return seg[x];
-        int mid = lx + (rx - lx) / 2;
-        return max(query(l, r, 2 * x + 1, lx, mid), query(l, r, 2 * x + 2, mid + 1, rx));
-    }
-    
     int lengthOfLIS(vector<int>& nums, int k) {
+        int n = nums.size();
         int x = 1;
         while(x <= 200000) x *= 2;
-        seg.resize(2 * x, 0);
         
-        int res = 1;
-        for(int i = 0; i < nums.size(); ++i) {
-            int left = max(1, nums[i] - k), right = nums[i] - 1;
-            int q = query(left, right, 0, 0, x - 1); // check for the element in the range of [nums[i] - k, nums[i] - 1] with the maximum value
-            res = max(res, q + 1);
-            upd(nums[i], q + 1, 0, 0, x - 1); //update current value
+        
+        vector<int> tree(2*x, 0);
+        int res=0;
+        for(auto it: nums)
+        {
+            int left = max(1, it-k);
+            int right = it-1;
+            int curr = 1+query(0,x-1,left, right, 0, tree);
+            res = max(res, curr);
+            update(0, x-1, it, curr, 0, tree);
         }
+        
+        //for(int i=0; i<20; i++) cout<<tree[i]<<" ";
         return res;
     }
 };
